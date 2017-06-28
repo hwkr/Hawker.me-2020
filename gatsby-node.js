c0d8b8ae97b27e48ea37134e1acd47444cc60e44
Brynn Hawker <brynn@hawker.me>
2017-06-27 22:14:58 -0700

    Tweak loader

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@
 exports.modifyWebpackConfig = function (config, stage) {
   config.loader('css', function (cfg) {
     cfg.test = /\.font\.?(js|json)$/;
-    cfg.loader = 'style!css!less-loader!webfonts';
+    cfg.loader = 'style!css!less!webfonts';
     return cfg;
   });
   return config;
