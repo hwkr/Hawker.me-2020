939253e9dd3aee17fbd6ab97edd564c7f37cc754
Brynn Hawker <brynn@hawker.me>
2017-11-23 16:30:27 -0800

    Add wishlist

---
+++ src/pages/wishlist.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class WishlistPage extends Component {
+
+  static propTypes = {
+  }
+
+  render() {
+    return (
+      <div className="wishlist">
+        <div className="container grid-720">
+          <h1 className="mt-10">Brynn's Wishlist</h1>
+          <script src="https://gist.github.com/hwkr/9b67de673a4a353a448afd3a4fea580d.js" />
+        </div>
+      </div>
+    );
+  }
+}
+
