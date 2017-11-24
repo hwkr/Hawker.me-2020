8529cd6a49de00893d585ba82dedae3c94d9e3a1
Brynn Hawker <brynn@hawker.me>
2017-11-23 17:07:38 -0800

    Tweak wishlist

--- src/pages/wishlist.js
+++ src/pages/wishlist.js
@@ -8,8 +8,7 @@ export default class WishlistPage extends Component {
   render() {
     return (
       <div className="wishlist">
-        <div className="container grid-720">
-          <h1 className="mt-10">Brynn's Wishlist</h1>
+        <div className="container mt-10 grid-720">
           <script src="https://gist.github.com/hwkr/9b67de673a4a353a448afd3a4fea580d.js" />
         </div>
       </div>
