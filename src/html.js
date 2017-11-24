7adc4bc3b7ea36958b9aeca4cacbe92cc209d623
Brynn Hawker <brynn@hawker.me>
2017-11-23 16:56:45 -0800

    Fix favicon

--- src/html.js
+++ src/html.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 
-import favicon from 'file-loader!./img/favicon.png';
+import favicon from '!!file-loader!./img/favicon.png';
 
 const BUILD_TIME = new Date().getTime();
 
