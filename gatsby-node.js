1fa08375e6c5622da22119a498df792f45c38a91
Brynn Hawker <brynn@hawker.me>
2019-05-17 21:39:46 -0700

    Fix production build

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,30 +7,22 @@ exports.onCreateWebpackConfig = ({
   plugins,
   actions,
 }) => {
-  actions.setWebpackConfig({
-    module: {
-      rules: [
-        {
-          test: /\.font\.?(js|json)$/,
-          use: [
-            // We don't need to add the matching ExtractText plugin
-            // because gatsby already includes it and makes sure its only
-            // run at the appropriate stages, e.g. not in development
-            loaders.miniCssExtract(),
-            loaders.css({ importLoaders: 1 }),
-            // the postcss loader comes with some nice defaults
-            // including autoprefixer for our configured browsers
-            loaders.postcss(),
-            'less-loader',
-            'webfonts-loader',
-          ],
-        },
-      ],
-    },
-    plugins: [
-      plugins.define({
-        __DEVELOPMENT__: stage === 'develop' || stage === 'develop-html',
-      }),
-    ],
-  });
+  if (stage === 'develop' || stage === 'build-javascript') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /\.font\.?(js|json)$/,
+            use: [
+              loaders.miniCssExtract(),
+              loaders.css({ importLoaders: 1 }),
+              loaders.postcss(),
+              'less-loader',
+              'webfonts-loader',
+            ],
+          },
+        ],
+      },
+    });
+  }
 };
