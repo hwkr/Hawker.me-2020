d9bf2c8f5397df468161e16668ff8ac605fa5142
Brynn Hawker <brynn@hawker.me>
2019-05-31 16:01:17 -0700

    Fiz titles

--- src/components/parts/Layout.js
+++ src/components/parts/Layout.js
@@ -48,7 +48,7 @@ export default class Layout extends Component {
           console.log(...console_cats);
           return (
             <div className={className}>
-              <Helmet titleTemplate={`%s | ${title}`} defaultTitle={title} />
+              <Helmet titleTemplate={`%s → ${title}`} defaultTitle={title} />
               { children }
               <Footer socialLinks={social_links} />
             </div>
